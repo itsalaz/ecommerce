0e214a7d2919bfa8d83198f90c9e79b0eaef27e8
Elizabeth <itsalaz@icloud.com>
2024-07-07 13:40:21 -0400

    worked on product details

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,8 +1,16 @@
 import React from "react"
 
 
-export default function ProductItem() {
+export default function ProductItem({product}) {
+
   return (
+    <li className="product-item">
+      <Link to={`/products/${product.id}`}>
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <h3>{product.price}</h3>
+      </Link>
+    </li>
     
   )
 }
--- pages/ProductDetails.js
+++ pages/ProductDetails.js
@@ -2,8 +2,31 @@ import React from "react"
 import { useParams } from "react-router-dom"
 
 
-export default function ProductDetails() {
-
+export default function ProductDetails({product}) {
+  function handleAddToCartClick() {
+    console.log("clicked item", item)
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !product.isInCart,
+      }),
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json()
+    })
+    .then((updatedProduct) => {
+      onUpdateProduct(updatedProduct)
+    })
+    .catch((error) => {
+      console.error("Error updating item:", error)
+    })
+  }
   
   return (
     <div className="product-details">
@@ -14,7 +37,10 @@ export default function ProductDetails() {
         <h1>{product.name}</h1>
         <p>{product.description}</p>
         <p>{product.price}</p>
-        <button className="add-to-cart">Add To Cart</button>
+        <button className={product.isInCart ? "Added To Cart" : "Add To Cart"}
+          onClick={handleAddToCartClick}>
+            {product.isInCart ? "In Cart" : "Add To Cart"}Add To Cart
+        </button>
       </div>
     </div>
   )
